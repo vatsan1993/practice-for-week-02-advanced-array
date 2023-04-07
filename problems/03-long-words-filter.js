bf58c551ca7d68b86dc56e0067de83939f1151af
Srivatsan <vatsan1993@gmail.com>
2023-04-07 17:18:45 +0530

    finished 4th program

--- problems/03-long-words-filter.js
+++ problems/03-long-words-filter.js
@@ -14,7 +14,8 @@ console.log(longWords(['couscous', 'soup', 'ceviche', 'solyanka' ,'taco'])); // 
 */
 
 let longWords = function(words) {
-    // Your code here
+    let result = words.filter(value => value.length > 5);
+    return result
 };
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
@@ -23,4 +24,4 @@ try {
   module.exports = longWords;
 } catch (e) {
   module.exports = null;
-}
+}

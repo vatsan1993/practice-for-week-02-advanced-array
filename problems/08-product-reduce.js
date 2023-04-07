b780c99415ae8faf43d916a9c85c156c9a573346
Srivatsan <vatsan1993@gmail.com>
2023-04-07 18:14:21 +0530

    finished program 9

--- problems/08-product-reduce.js
+++ problems/08-product-reduce.js
@@ -16,7 +16,9 @@ console.log(productWithReduce([4, 3])); // 12
 */
 
 let productWithReduce = function(nums) {
-    // Your code here
+  let initialValue = 1;
+    let product = nums.reduce((acc, value) => acc * value, initialValue);
+    return product;
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
@@ -25,4 +27,4 @@ try {
   module.exports = productWithReduce;
 } catch (e) {
   module.exports = null;
-}
+}
